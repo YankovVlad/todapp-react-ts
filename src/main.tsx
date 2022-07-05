ea3358d8c4fc027661c7d1f30af4aef7a8eac245
VladYankov <vladankov996@gmail.com>
2022-07-05 13:00:22 +0300

    feature/redux - implemented redux

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './app/App'
 import './index.css'
 
+import { store } from './app/store/store'
+import { Provider } from 'react-redux'
+
 import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+    
   </React.StrictMode>
 )
